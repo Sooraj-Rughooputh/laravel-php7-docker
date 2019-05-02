7de5273bc181abf0e928d14597057371d6e839ca
Sooraj Rughooputh <sooraj.rughooputh@mvfglobal.com>
2019-05-02 09:54:20 +0100

    Removing git branch shown / changing name of project

--- application/routes/web.php
+++ application/routes/web.php
@@ -12,17 +12,5 @@
 */
 
 Route::get('/', function () {
-
-    $git_branch = '';
-    $head_file = '/GIT_BRANCH_INFO';
-
-    if (file_exists($head_file)) {
-        $git_branch = file_get_contents($head_file);
-        $git_branch = explode('/', $git_branch);
-        $git_branch = end($git_branch);
-    }
-
-    return view('welcome', [
-      'git_branch_name' => $git_branch,
-    ]);
+    return view('welcome');
 });
